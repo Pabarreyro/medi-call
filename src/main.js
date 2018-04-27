77b074a2acf8c01ba0f9bd83a0dca950f83d7286
Pablo Barreyro <Pabarreyro@gmail.com>
2018-04-27 11:19:26 -0700

    add code for testing queryCall and nameCall methods

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
-// import $ from 'jquery';
-// import { function } from './filename';
+import $ from 'jquery';
+import { MediCall } from './medi-call.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+
+
+$(function() {
+  let newCall = new MediCall();
+
+  let queryPromise = newCall.queryCall("asthma");
+  // let namePromise = newCall.nameCall("jones");
+
+  queryPromise.then(function(response){
+    console.log(response);
+  });
+});
