8a55579e9017cbceb3856668656cc581476a298a
Pablo Barreyro <Pabarreyro@gmail.com>
2018-04-27 11:21:37 -0700

    add response limit as parameter of queryCall and nameCall methods

--- src/medi-call.js
+++ src/medi-call.js
@@ -1,9 +1,9 @@
 class MediCall {
-  queryCall(query){
+  queryCall(query, limit){
     let promise;
     return promise = new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${ query }&location=or-portland&skip=0&limit=10&user_key=${ process.env.exports.apiKey }`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${ query }&location=or-portland&skip=0&limit=${ limit }&user_key=${ process.env.exports.apiKey }`;
       request.responseType = 'json';
       request.onload = function() {
         if (this.status === 200) {
@@ -18,11 +18,11 @@ class MediCall {
     });
   }
 
-  nameCall(name){
+  nameCall(name, limit){
     let promise;
     return promise = new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?last_name=${ name }&location=or-portland&skip=0&limit=10&user_key=${ process.env.exports.apiKey }`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?last_name=${ name }&location=or-portland&skip=0&limit=${ limit }&user_key=${ process.env.exports.apiKey }`;
       request.responseType = 'json';
       request.onload = function() {
         if (this.status === 200) {
