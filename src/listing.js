66090eb451b87701842b504a46a43e5958503ef8
Pablo Barreyro <Pabarreyro@gmail.com>
2018-04-28 18:22:53 -0700

    add class for storing json response objects

---
+++ src/listing.js
@@ -0,0 +1,17 @@
+class Listing {
+  constructor(responseObj, index) {
+    this.id = (index +1).toString();
+    this.photo = responseObj.profile.image_url;
+    this.firstName = responseObj.profile.first_name;
+    this.lastName = responseObj.profile.last_name;
+    this.title = responseObj.profile.title;
+    this.specialty = responseObj.specialties[0].name;
+    this.practice = responseObj.practices[0].name;
+    this.address = [responseObj.practices[0].visit_address.street,
+                    responseObj.practices[0].visit_address.city, responseObj.practices[0].visit_address.state, responseObj.practices[0].visit_address.zip,];
+    this.phone = [responseObj.practices[0].phones[0].number,
+                  responseObj.practices[0].phones[0].type ];
+  }
+}
+
+export { Listing };
